7061b3629b2a615d36ef04f4d71f60551714f7f4
NikolausYN <nikolaus.yesua@gmail.com>
2020-12-13 20:50:36 +0700

    new project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="homepage text-center">
+        <div class="atas text-center" >
+            <h1 class="judul">Welcome to Flowelto Shop</h1>
+            <h4 class="subjudul">The Best Flower Shop in Binus University</h4>
+        </div>
+    </div>
+</div>
+@endsection
